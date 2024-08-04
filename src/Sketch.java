b35c4f4e196801978572932cfe5673743c16ff9c
Renan Silva <renansupermanox@gmail.com>
2024-08-04 20:30:41 +0100

    some functions on world and update on sketch

--- src/Sketch.java
+++ src/Sketch.java
@@ -6,7 +6,9 @@ public class Sketch extends PApplet {
     private static final int COLS = 40;
     private static final int ROWS = 40;
     private static final float OFFSET_X = (float)WIDTH/COLS;
-    private static final float OFFSET_Y = (float)HEIGHT/COLS;
+    private static final float OFFSET_Y = (float)HEIGHT/ROWS;
+
+    private final World world = new World(COLS, ROWS);
 
     public static void main(String[] args) {
         PApplet.main("Sketch");
@@ -27,9 +29,17 @@ public class Sketch extends PApplet {
         background(0);
 
         for (int i = 0; i < COLS + 1; i++) {
-
             line(i * OFFSET_X, 0, i * OFFSET_X, HEIGHT);
             line(0, i * OFFSET_Y, WIDTH, i * OFFSET_Y);
+        }
+
+        world.updateCells();
+
+        for (int i = 0; i < COLS + 1; i++) {
+            for (int j = 0; j < ROWS; j++) {
+                if(isAlive(i, j))
+                    rect(i * OFFSET_X, j * OFFSET_Y, OFFSET_X, HEIGHT);
+            }
         }
     }
 }
--- src/World.java
+++ src/World.java
@@ -5,13 +5,32 @@ public class World {
     private static final int MIN_OVERPOPULATION = 3;
     private static final int MAX_UNDERPOPULATION = 2;
 
+    private final int rows;
+    private final int cols;
     private final boolean[][] grid;
 
     public World(int cols, int rows) {
+        this.rows = rows;
+        this.cols = cols;
         grid = new boolean[cols][rows];
     }
 
     public void updateCells() {
 
+    }
+
+    public boolean isAlive(int row, int col) {
+        return grid[col][row];
+    }
+
+    private void update() {
+        boolean[][] newGrid = new boolean[rows][cols];
+
+        for (int col = 0; col < cols; col++)
+            for (int row = 0; row < rows; row++)
+                if(willLive(col, row))
+                    newGrid[col][row] = true;
+
+        grid = newGrid;
     }
 }
