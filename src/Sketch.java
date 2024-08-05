2f410feaaf12a46f10e3460744b645a5963b1732
Renan Silva <renansupermanox@gmail.com>
2024-08-05 12:57:22 +0100

    change in colors, glider gun layout added and some changes in code

--- src/Sketch.java
+++ src/Sketch.java
@@ -19,29 +19,30 @@ public class Sketch extends PApplet {
     }
 
     public void setup() {
-        stroke(255);
-        strokeWeight(0.5f);
-        fill(255);
-        frameRate(3);
+        stroke(200);
+        fill(0);
+        frameRate(5);
     }
 
     public void draw() {
-        background(0);
+        background(255);
+        drawLines();
+        world.updateCells();
+        drawAliveCells();
+    }
 
-        for (int i = 0; i < COLS; i++) {
+    private void drawLines() {
+        for (int i = 0; i < COLS; i++)
             for (int j = 0; j < ROWS; j++) {
                 line(i * OFFSET_X, 0, i * OFFSET_X, HEIGHT);
                 line(0, j * OFFSET_Y, WIDTH, j * OFFSET_Y);
             }
-        }
+    }
 
-        world.updateCells();
-
-        for (int i = 0; i < ROWS; i++) {
-            for (int j = 0; j < COLS; j++) {
+    private void drawAliveCells() {
+        for (int i = 0; i < ROWS; i++)
+            for (int j = 0; j < COLS; j++)
                 if(world.isAlive(i, j))
-                    rect(i * OFFSET_X, j * OFFSET_Y, OFFSET_X, OFFSET_Y);
-            }
-        }
+                    rect(j * OFFSET_X, i * OFFSET_Y, OFFSET_X, OFFSET_Y);
     }
 }
--- src/World.java
+++ src/World.java
@@ -2,8 +2,8 @@ public class World {
     private static final int NEIGHBOURS_TO_REVIVE = 3;
     private static final int MIN_NEIGHBOURS_STAY_ALIVE = 2;
     private static final int MAX_NEIGHBOURS_STAY_ALIVE = 3;
-    private static final int MIN_OVERPOPULATION = 3;
-    private static final int MAX_UNDERPOPULATION = 2;
+//    private static final int MIN_OVERPOPULATION = 3;
+//    private static final int MAX_UNDERPOPULATION = 2;
 
     private final int rows;
     private final int cols;
@@ -14,31 +14,48 @@ public class World {
         this.cols = cols;
         grid = new boolean[rows][cols];
 
-        // Calcula a posição central
-        int centerX = 20;
-        int centerY = 20;
+        // ChatGPT:
+        int[][] gliderGun = {
+                {1, 25},
+                {2, 23}, {2, 25},
+                {3, 13}, {3, 14}, {3, 21}, {3, 22}, {3, 35}, {3, 36},
+                {4, 12}, {4, 16}, {4, 21}, {4, 22}, {4, 35}, {4, 36},
+                {5, 1},  {5, 2},  {5, 11}, {5, 17}, {5, 21}, {5, 22},
+                {6, 1},  {6, 2},  {6, 11}, {6, 15}, {6, 17}, {6, 18}, {6, 23}, {6, 25},
+                {7, 11}, {7, 17}, {7, 25},
+                {8, 12}, {8, 16},
+                {9, 13}, {9, 14}
+        };
 
-        // Posiciona o glider no meio da matriz
-        grid[centerY - 1][centerX] = true;   // O
-        grid[centerY][centerX + 1] = true;   // O
-        grid[centerY + 1][centerX - 1] = true; // O
-        grid[centerY + 1][centerX] = true;   // O
-        grid[centerY + 1][centerX + 1] = true;
+        for (int[] coord : gliderGun) {
+            int x = coord[0];
+            int y = coord[1];
+            grid[x][y] = true;
+        }
     }
 
     public boolean willLive(int row, int col) {
         boolean alive = grid[row][col];
         int neighbours = countNeighbours(row, col);
 
-        if (alive && neighbours < MAX_UNDERPOPULATION) return false;
+        if(alive) {
+            if(neighbours < MIN_NEIGHBOURS_STAY_ALIVE)
+                return false;
+            else
+                return neighbours <= MAX_NEIGHBOURS_STAY_ALIVE;
+        } else {
+            return neighbours == NEIGHBOURS_TO_REVIVE;
+        }
 
-        else if (alive && neighbours >= MIN_NEIGHBOURS_STAY_ALIVE && neighbours <= MAX_NEIGHBOURS_STAY_ALIVE) return true;
-
-        else if (alive && neighbours > MIN_OVERPOPULATION) return false;
-
-        else if (!alive && neighbours == NEIGHBOURS_TO_REVIVE) return true;
-
-        else return false;
+//        if (alive && neighbours < MAX_UNDERPOPULATION) return false;
+//
+//        else if (alive && MIN_NEIGHBOURS_STAY_ALIVE <= neighbours && neighbours <= MAX_NEIGHBOURS_STAY_ALIVE) return true;
+//
+//        else if (alive && neighbours > MIN_OVERPOPULATION) return false;
+//
+//        else if (!alive && neighbours == NEIGHBOURS_TO_REVIVE) return true;
+//
+//        else return false;
     }
 
     private int countNeighbours(int row, int col) {
